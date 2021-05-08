3c7a479b975b8d02f4b82c830bdfd066e9a3725e
gnujach <julian.abarca@gmail.com>
2021-05-08 12:54:50 -0500

    Cambiamos la consulta de currencies

--- app/Http/Controllers/V1/General/CurrenciesController.php
+++ app/Http/Controllers/V1/General/CurrenciesController.php
@@ -17,7 +17,7 @@ class CurrenciesController extends Controller
     public function __invoke(Request $request)
     {
         // $currencies = Currency::latest()->get();
-        $currencies = Currency::where('id', 29)->get();
+        $currencies = Currency::where('id', 1)->get();
 
         return response()->json([
             'currencies' => $currencies
